705bdf6a4f03cfc62756598c515a5408d4042acf
Hong Luu <hluu114@gmail.com>
2017-07-18 21:39:23 -0700

    Initial Push

---
+++ demorest/src/main/java/com/jersey/demorest/UserDao.java
@@ -0,0 +1,17 @@
+package com.jersey.demorest;
+
+import java.util.ArrayList; 
+import java.util.List;  
+
+public class UserDao { 
+   public List<User> getAllUsers(){ 
+      
+		List<User> userList = null; 
+		User user = new User(1, "Name01", "Teacher01"); 
+		User user2 = new User(2, null, "Teacher02"); 
+		userList = new ArrayList<User>(); 
+		userList.add(user); 
+		userList.add(user2); 
+		return userList; 
+   } 
+}
---
+++ demorest/src/main/java/com/jersey/demorest/UserService.java
@@ -0,0 +1,29 @@
+package com.jersey.demorest;
+
+import java.util.List; 
+import javax.ws.rs.GET; 
+import javax.ws.rs.Path; 
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.GenericEntity;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriInfo;
+
+
+@Path("/UserService") 
+public class UserService {  
+	UserDao userDao = new UserDao();  
+	@GET 
+	@Path("/users")
+	@Produces({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON}) 
+	public Response getUsers(@Context UriInfo uriInfo){
+		GenericEntity<List<User>> entity = new GenericEntity<List<User>>(userDao.getAllUsers()){};
+		String format = uriInfo.getQueryParameters().getFirst("format");
+		if (format == null){
+			System.out.println("format is null");
+		}
+		return Response.ok(entity, format==null? MediaType.APPLICATION_XML : MediaType.APPLICATION_JSON)
+				.build();
+	}
+}
